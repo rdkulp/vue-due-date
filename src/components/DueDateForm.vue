a1ba850c0a6d17a2633c2ece973b0d0278b0c3d9
rdkulp <rdkulp@gmail.com>
2021-10-29 04:16:06 -0600

    hook up input on due-date-form component

---
+++ src/components/DueDateForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="due-date-form-wrapper">
+    <h1>Due Date Conversion</h1>
+    <input placeholder="3/20/2022" v-model="date">
+    <p>{{ date }}</p>
+  </section>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        date: '3/20/2022'
+      };
+    },
+  }
+</script>
+
+<style lang="scss">
+
+</style>
